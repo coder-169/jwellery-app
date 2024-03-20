c3fc876a88547cec9125b9dec2873492de0ef159
Saad <113129396+coder-169@users.noreply.github.com>
2024-03-20 07:07:15 +0500

    Push

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "../../public/assets/footerLogo.png";
+import Head from "next/head";
 
 const Header = () => {
   const [isActive, setisActive] = useState(false);
@@ -11,9 +12,46 @@ const Header = () => {
   };
   return (
     <>
-      <div className="h-20 flex justify-between items-center sm:bg-transparent nav px-2 lg:px-[5vw]">
-        <div className="">
-          <Image src={Logo} width={250} height={100} />
+      <header className="h-20 flex justify-between items-center sm:bg-transparent nav px-2 lg:px-[5vw]">
+        <Head>
+        <meta
+            name="description"
+            content="Diamond Labs combines crafting, engineering and data technologies to create, design and trade your perfect pieces of jewellery and then, immortalizing them.  
+            Legacy, made perfect."
+          />
+          <meta name="robots" content="max-image-preview:large" />
+          <meta property="og:locale" content="en_US" />
+          <meta
+            property="og:site_name"
+            content="Diamond Labs"
+          />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:title"
+            content="Diamond Labs"
+          />
+          <meta
+            property="og:description"
+            content="I am an Experienced Full stack web developer and blogger I create stunning and user-centric online experiences with seamless let&#039;s build your online presence."
+          />
+
+          <meta property="og:image:width" content="1536" />
+          <meta property="og:image:height" content="2048" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@diamondlabs" />
+          <meta
+            name="twitter:title"
+            content="Diamond Labs"
+          />
+          <meta
+            name="twitter:description"
+            content="Diamond Labs combines crafting, engineering and data technologies to create, design and trade your perfect pieces of jewellery and then, immortalizing them.  
+            Legacy, made perfect."
+          />
+          <meta name="twitter:creator" content="@diamondlabs" />
+        </Head>
+        <div  className="">
+          <Image alt="" src={Logo} width={250} height={100} />
         </div>
         <div className="links flex font-semibold gap-[3vw] lg:text-xl">
           <Link className="text-sm" href="https://thepeak.io/">
@@ -34,9 +72,9 @@ const Header = () => {
         >
           CONTACT
         </a>
-      </div>
+      </header>
       <div className="h2 p-8 flex justify-center top-14 sm:top-0 absolute w-full">
-        <Image src={Logo} width={200} height={100} className="mx-auto" />
+        <Image alt="" src={Logo} width={200} height={100} className="mx-auto" />
       </div>
       <button
         onClick={navBar}
